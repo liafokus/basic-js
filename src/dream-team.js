4948272b62b200ea02ecbb4f96dfa9cff84e2ac1
Ольга Курьянович <macbook@MacBooks-MacBook-Pro.local>
2022-05-02 22:13:39 +0300

    complite task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+let members = [];
+function createDreamTeam(members) {
+  if (!Array.isArray(members) || !members.length){
+    return false;
+  }
+  let arrFirstLettersOfMember = [];//новый массив с первыми буквами
+  for(let i = 0; i < members.length; i++){
+    if(typeof members[i] === 'string'){ //проверка на string
+      let firstLetter = members[i].trimStart().charAt(0); //отделила первые буквы, убрала пробелы
+      arrFirstLettersOfMember.push(firstLetter.toUpperCase()); //запушила в новый массив
+    }
+  }
+  return arrFirstLettersOfMember.length > 0 
+  ? arrFirstLettersOfMember.sort().join('') 
+  : false;
 }
 
 module.exports = {
